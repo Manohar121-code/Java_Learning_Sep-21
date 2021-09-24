c8ddc0fe0d5c38bb6e5d83455a1feca9969207fa
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-09-24 09:12:35 +0530

    Pattern_Programming

---
+++ src/learn/basics/patternProgramming/Pattern2.java
@@ -0,0 +1,20 @@
+package learn.basics.patternProgramming;
+
+//54321
+//54321
+//54321
+//54321
+//54321
+public class Pattern2 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) { // rows
+
+			for (int j = 5; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+}
